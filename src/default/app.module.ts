497f3c7149189ccd91cf8ccf96ff5183bcd29576
HOME\MYHOME <curong@outlook.kr>
2023-03-20 23:53:19 +0900

    [FIXED] TypeORM config & reinstall...

--- src/default/app.module.ts
+++ src/default/app.module.ts
@@ -3,13 +3,18 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from 'src/config/typeorm/orm.config';
 
 @Module({
   imports: [
+    // GraphQL Using //
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+    // TypeORM Using //
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
     BoardModule,    
   ],
   controllers: [],
